d80fbe9b67c195421447a2152950478d74c60ff1
docilejustin <63971214+docilejustin@users.noreply.github.com>
2026-06-15 13:43:59 +0200

    small changes to info it gets

--- Python Scripts/FinalCode/DMRConnectLib.py
+++ Python Scripts/FinalCode/DMRConnectLib.py
@@ -21,5 +21,38 @@ def dmrlookup(plate):
         return None
 
     return {
+        # Identity & registration
+        "make": vehicle.make,
+        "model": vehicle.model,
+        #"variant": vehicle.variant,
+        #"vin": vehicle.vin,
+        #"registration_number": vehicle.registration_number,
+        #"vehicle_id": vehicle.vehicle_id,
+        "type": vehicle.type,
+        #"use": vehicle.use,
+
+        # Dates
+        #"first_registration": vehicle.first_registration,
+        #"last_update": vehicle.last_update,
+
+        # Body & physical
+        #"color": vehicle.color,
+        "model_year": vehicle.model_year,
+        "body_type": vehicle.body_type,
+        "doors": vehicle.doors,
+        "total_weight": vehicle.total_weight,
+        #"vehicle_weight": vehicle.vehicle_weight,
+        #"tow_bar": vehicle.tow_bar,
+
+        # Powertrain & emissions
         "powertrain": vehicle.propulsion,
+        "cylinders": vehicle.cylinders,
+        "fuel_consumption": vehicle.fuel_consumption,
+        "particle_filter": vehicle.particle_filter,
+
+        # Electric / hybrid
+        "plugin_hybrid": vehicle.plugin_hybrid,
+        "electricity_consumption": vehicle.electricity_consumption,
+        "electric_range": vehicle.electric_range,
+        "battery_capacity": vehicle.battery_capacity,
     }
